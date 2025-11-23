403c39d3495bcb2f53e42f0879fe1aeb6bb8b55f
Shridhar Pandey <shridharpandeyofficial@gmail.com>
2025-11-24 02:39:07 +0530

    handle useSearchParams with Suspense

--- app/(auth)/sign-in/reset-password/page.tsx
+++ app/(auth)/sign-in/reset-password/page.tsx
@@ -1,5 +1,17 @@
+import { Suspense } from "react";
 import { ResetPasswordForm } from "@/components/reset-password-form";
+import { Spinner } from "@/components/ui/spinner";
 
 export default async function Page() {
-  return <ResetPasswordForm />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex min-h-screen items-center justify-center">
+          <Spinner />
+        </div>
+      }
+    >
+      <ResetPasswordForm />
+    </Suspense>
+  );
 }
