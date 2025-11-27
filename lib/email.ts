4dd8811995ce946a4d2b9cbec0851179c513fae5
Shridhar Pandey <shridharpandeyofficial@gmail.com>
2025-11-27 10:59:01 +0530

    update nodemailer configuration

--- lib/email.ts
+++ lib/email.ts
@@ -6,7 +6,9 @@ export const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER as string,
     pass: process.env.EMAIL_PASS as string,
   },
-  secure: true,
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const sendEmail = async ({
